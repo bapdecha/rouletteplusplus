ea74c6dd3a01c6397605fe6c022b588b579081e1
Baptiste de CHARRY <baptiste.de-charry@epita.fr>
2026-03-03 17:09:23 +0100

    added IaC

--- main.tf
+++ main.tf
@@ -2,26 +2,49 @@ terraform {
   required_providers {
     ovh = {
       source  = "ovh/ovh"
-      version = "~> 0.35"
+      version = ">= 2.11.0"
     }
   }
 }
 
 provider "ovh" {
-  region             = "GRA7"
+  endpoint           = "ovh-eu"
   application_key    = var.ovh_app_key
   application_secret = var.ovh_app_secret
   consumer_key       = var.ovh_consumer_key
 }
 
+data "ovh_cloud_project_images" "image" {
+  service_name = var.ovh_project_id
+  region = "GRA11"
+  os_type = "linux"
+}
+
+data "ovh_cloud_project_flavors" "flavor" {
+  service_name = var.ovh_project_id
+  region = "GRA11"
+  name_filter = "d2-2"
+}
+
 resource "ovh_cloud_project_instance" "docker_server" {
+  region       = "GRA11"
   service_name = var.ovh_project_id
   name         = "docker-host-ghcr"
-  flavor_name  = "s1-2" # 2GB RAM, idéal pour débuter
-  image_name   = "Ubuntu 22.04"
-  key_pair     = "ma-cle-ssh"
+  flavor {
+    flavor_id  = [for f in data.ovh_cloud_project_flavors.flavor.flavors : f.id if f.name == "d2-2"][0]
+  }
+  boot_from {
+    image_id = [for i in data.ovh_cloud_project_images.image.images : i.id if i.name == "Ubuntu 25.04 - UEFI"][0]
+  }
+  ssh_key {
+    name = "ma cle"
+  }
+  network {
+    public = true
+  }
+  billing_period = "hourly"
 }
 
 output "server_ip" {
-  value = ovh_cloud_project_instance.docker_server.public_ip
+  value = ovh_cloud_project_instance.docker_server.addresses
 }
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ovh_app_key" {
+    type = string
+}
+
+variable "ovh_app_secret" {
+    type = string
+}
+
+variable "ovh_consumer_key" {
+    type = string
+}
+
+variable "ovh_project_id" {
+    type = string
+}
